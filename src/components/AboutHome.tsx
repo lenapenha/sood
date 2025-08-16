638f6faa3ec6cd9c170e81b4cc0ddbbdbb731d65
Helena Penha <lenapenha02@gmail.com>
2025-08-15 22:40:02 -0500

    about details

--- src/components/AboutHome.tsx
+++ src/components/AboutHome.tsx
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import Cross from '@/assets/cross.png';
+import Heart from '@/assets/heart.png';
+import Dove from '@/assets/dove.png';
+
 export default function AboutHome() {
     return (
         <section id="about" className="container flex flex-col justify-center items-center py-16">
@@ -12,36 +17,73 @@ export default function AboutHome() {
                 <div className="lg:col-span-2 p-4 rounded-lg shadow-md">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div className="p-4 rounded-lg shadow text-center">
+                            <Image src={Cross} alt="Our Mission" width={100} height={100} className="mx-auto" />
                             <h3 className="text-xl font-bold mb-2">Our Mission</h3>
                             <p>
                                 To provide hope and support to those struggling with addiction and mental health issues.
-                                 It is our goal to help with spiritual, emotional, and physical healing.
+                                 It is our goal to help with spiritual, emotional, and physical healing. To see every hurting person find healing, hope, and freedom, never walking alone through addiction, trauma, or homelessness.
                             </p>
                         </div>
                         <div className="p-4 rounded-lg shadow text-center">
+                            <Image src={Heart} alt="Our Vision" width={100} height={100} className="mx-auto" />
                             <h3 className="text-xl font-bold mb-2">Our Vision</h3>
                             <p>
-                                A world where individuals can find healing and restoration through Christ.
+                               To see every hurting person find healing, hope, and freedom. Never walking alone through addiction, trauma, or homelessness.
                             </p>
                         </div>
                     </div>
                     <div className="text-center">
+                        <Image src={Dove} alt="Our Values" width={100} height={100} className="mx-auto" />
                         <h3 className="text-xl font-bold mb-2">Our Values</h3>
-                        <p>
-                            Compassion, Integrity, and Empowerment.
-                        </p>
+                        <ul className="list-disc list-inside text-left inline-block">
+                            <li><strong>Faith:</strong> Christ is at the center of everything we do.</li>
+                            <li><strong>Compassion:</strong> We love people right where they are.</li>
+                            <li><strong>Healing:</strong> Emotional, spiritual, and physical restoration.</li>
+                            <li><strong>Community:</strong> No one heals alone; we walk together.</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="p-4 rounded-lg shadow-md text-center">
-                    <h3 className="text-xl font-bold mb-2">Michelle</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec enim diam, suscipit id convallis vel, pharetra sodales augue. Maecenas lacinia, erat sed interdum convallis, dolor arcu dignissim sapien, in euismod augue ex sit amet ipsum. Phasellus in metus a purus blandit tincidunt. Vestibulum nec felis ac quam rutrum molestie eu eget ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    <h3 className="text-3xl font-serif mb-2 lg:mb-8">Michelle Perry</h3>
+                    <p className="mb-2 lg:mb-8">
+                        She grew up experiencing significant trauma and eventually went down a path of drug addiction and deep emotional wounds. She later found healing and hope through her faith in God and now leads Straight Out of Darkness Ministry, where she shares her story to inspire others on their own journey of recovery from addiction and trauma.
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec enim diam, suscipit id convallis vel, pharetra sodales augue. Maecenas lacinia, erat sed interdum convallis, dolor arcu dignissim sapien, in euismod augue ex sit amet ipsum.
+                        Michelle faced many challenges at home due to her parents’ struggles with addiction, alcoholism, domestic violence, and mental health issues. Despite the abuse, she persevered with resilience and found restoration through her faith. She is now actively involved in Straight Out of Darkness Recovery Ministry and desires to be a voice for those struggling with mental illness, addiction, domestic violence, and homelessness.
                     </p>
                 </div>
             </div>
+        <div className="container flex flex-col lg:flex-row justify-between items-start p-4 lg:py-16 gap-8">    
+            <div>
+                <h3 className="text-3xl font-serif mb-2 lg:mb-8">Where we serve</h3>
+                <p className="mb-2  ">
+                    Thursdays | 6:00-7:00 p.m.
+                </p>
+                <p className="mb-2">
+                    At Hope and Anchor Church: 2216 W College, Springfield, MO
+                </p>
+                <p className="mb-2 lg:mb-8">
+                    Childcare is not available at this time.
+                </p>
+                <p>
+                    Our meetings are open share groups for anyone struggling with addiction, alcoholism, mental health challenges, grief, or trauma. As well as anyone who has loved ones going through these issues and wants to learn how to support them.
+
+                </p>
+            </div>
+            <div>
+                <h3 className="text-3xl font-serif mb-2 lg:mb-8">Get Involved</h3>
+                <p className="mb-2 lg:mb-8">
+                    There are many ways to get involved with our ministry and support our mission. Whether you want to volunteer your time, make a donation, or simply spread the word, we welcome your support.
+                </p>
+                <p>
+                    Together, we can make a difference in the lives of those struggling with addiction and trauma. Contact us today to learn more about how you can help.
+                </p>
+            </div>
+        </div>
+        <div className="mt-8">
+            <h3 className="text-3xl font-serif font-bold mb-2 lg:mb-8">Ready to take the first step?</h3>
+
+        </div>
         </section>
     );
 }
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-50 border-t">
+    <footer className="bg-gray-900 border-t">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-end items-center">
-          <div className="text-gray-500 text-sm">
+          <div className="text-white text-sm">
             © 2025 Straight Out Of Darkness. All rights reserved.
           </div>
         </div>
